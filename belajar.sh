bc6cc18f053429311a52b5321c1ce2dda40bc579
IndonesiaJaya720 <72527640+IndonesiaJaya720@users.noreply.github.com>
2020-12-25 22:56:45 +0700

    Add files via upload

---
+++ belajar.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+#
+# Copyright (C) 2016 Rizki Mufrizal <mufrizalrizki@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+Year=`date +%Y`
+Month=`date +%m`
+Day=`date +%d`
+Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo
+sleep 1
+clear
+echo " `date` "
+echo " \033[31;1m████╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬████ "
+echo " ██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬███████╬╬╬╬╬╬╬╬╬███████╬╬╬█ "
+echo " █╬╬██╬╬╬╬███╬╬╬╬╬╬╬███╬╬╬╬██╬╬█ "
+echo " █╬██╬╬╬╬╬╬╬██╬╬╬╬╬██╬╬╬╬╬╬╬██╬█ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬╬█████╬╬╬╬╬╬╬╬╬╬╬█████╬╬╬╬█ "
+echo " █╬╬█████████╬╬╬╬╬╬╬█████████╬╬█ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬▓▓▓▓╬╬╬╬╬╬╬█╬╬╬╬╬╬╬▓▓▓▓╬╬╬█ "
+echo " █╬╬▓▓▓▓▓▓╬╬█╬╬╬█╬╬╬█╬╬▓▓▓▓▓▓╬╬█ "
+echo " █╬╬╬▓▓▓▓╬╬██╬╬╬█╬╬╬██╬╬▓▓▓▓╬╬╬█ "
+echo " \033[37;1m█╬╬╬╬╬╬╬╬██╬╬╬╬█╬╬╬╬██╬╬╬╬╬╬╬╬█ "
+echo " █╬╬╬╬╬████╬╬╬╬███╬╬╬╬████╬╬╬╬╬█ "
+echo " █╬╬╬╬╬╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬╬╬╬╬╬█ "
+echo " ██╬╬█╬╬╬╬╬╬╬╬█████╬╬╬╬╬╬╬╬█╬╬██ "
+echo " ██╬╬██╬╬╬╬╬╬███████╬╬╬╬╬╬██╬╬██ "
+echo " ██╬╬▓███╬╬╬████╬████╬╬╬███▓╬╬██ "
+echo " ███╬╬▓▓███████╬╬╬███████▓▓╬╬███ "
+echo " ███╬╬╬╬▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬███ "
+echo " ████╬╬╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬╬╬████ "
+echo " █████╬╬╬╬╬╬╬╬╬╬█╬╬╬╬╬╬╬╬╬╬█████ "
+echo " ██████╬╬╬╬╬╬╬╬███╬╬╬╬╬╬╬╬██████ "
+echo " ███████╬╬╬╬╬╬╬███╬╬╬╬╬╬╬███████ "
+echo " ████████╬╬╬╬╬╬███╬╬╬╬╬╬████████ "
+echo " █████████╬╬╬╬╬███╬╬╬╬╬█████████ "
+echo " ███████████╬╬╬╬█╬╬╬╬███████████ "
+echo " \033[1;34mAtthur : Mr_@LocalHost"
+echo " \033[1;32mgithub : IndonesiaJaya720"
+python2 hasil-decompiled.py
+sleep 1
+sleep 2
+sleep 3
+echo " \033[1;36mWa     : 0895392705209"
+python2 hasil.py
+sleep 1
+sleep 2
+sleep 3
+echo " Gmail : jonathanhasudungan32"
+echo
+python2 gmailsaya.py
+sleep 1
+sleep 2
+sleep 3
+echo " Facebook : "
+python2 FacebookSaya.py
+sleep 1
+sleep 2
+sleep 3
+echo " \033[1;35m[+]"" What Is Your Name : ""[+]"
+echo " \033[1;37m"
+read Name
+echo " \033[1;31m[×]"" Checking Your Name ""[×]"
+sleep 1
+sleep 2
+sleep 3
+echo " [✓]"" Hi, friend , ""$Name""[✓]"
+sleep 1
+sleep 2
+sleep 3
+clear
+echo
+sh IndonesiaJaya.sh
